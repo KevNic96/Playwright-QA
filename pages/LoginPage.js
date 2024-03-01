6d6362e10a5a6daf20fac0057e2e3f7dd2b39bfa
Kevin Flores <kevnicflores96@gmail.com>
2024-03-01 12:53:02 -0300

    Login Page n Spec fix

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,14 +1,29 @@
-class LoginPage {
-    constructor(page) {
-      this.page = page;
-    }
-  
-    async login(username, password) {
-      await this.page.fill('input[name="username"]', username);
-      await this.page.fill('input[name="password"]', password);
-      await this.page.click('input[type="submit"]');
-    }
+// @ts-check
+const { expect } = require('@playwright/test');
+
+module.exports = class LoginPage {
+
+  constructor(page) {
+    this.page = page;
+    this.username = page.getByLabel('Username', {exact: true});
+    this.password = page.getByLabel('Password', {exact: true});
+    this.loginButton = page.getByText('Login', { exact: true });
   }
-  
-  module.exports = LoginPage;
-  
+
+  async goto() {
+    await this.page.goto('https://frontend-training-taupe.vercel.app/login');
+  }
+
+  async login({ username, password }){
+    await this.username.fill(username)
+    await this.password.fill(password)
+    expect(username, await this.username.inputValue())
+    expect(password, await this.password.inputValue())
+    await this.loginButton.click()
+    await this.page.waitForURL('**/');
+  }
+
+}
+
+
+
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,22 +1,11 @@
-const { chromium } = require('playwright');
-const LoginPage = require('../pages/LoginPage');
+const { test, expect } = require('@playwright/test');
+const LoginPage = require("../pages/LoginPage");
 
-describe('Login tests', () => {
-  let browser, page, loginPage;
-
-  beforeAll(async () => {
-    browser = await chromium.launch();
-    page = await browser.newPage();
-    loginPage = new LoginPage(page);
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  it('should login successfully with valid credentials', async () => {
-    await loginPage.login('usuario', 'contraseña');
-    // Asegurar que el inicio de sesión fue exitoso
-  });
-
+test('User can register successfully', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto()
+  await loginPage.login({
+    username: 'Nico96',
+    password: 'Password123!',
+  })
 });
